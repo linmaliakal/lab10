3cb122558d3cc2eec5a6e84fc1757d8aab5b66eb
linmaliakal <lmaliakal@luc.edu>
2017-12-11 21:45:25 +0000

    fixed GJF, map created, need to traverse graph with different algs
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/edu/luc/cs271/countrygraph/CountryCodes.java
+++ src/main/java/edu/luc/cs271/countrygraph/CountryCodes.java
@@ -1,41 +1,39 @@
 package edu.luc.cs271.countrygraph;
 
-import java.util.*;
 import java.awt.*;
 import java.net.*;
+import java.util.*;
 import org.jgrapht.*;
 import org.jgrapht.alg.*;
+import org.jgrapht.graph.*;
 import org.jgrapht.traverse.*;
 
 public class CountryCodes {
 
-  final static String AR = "Argentina";
-  final static String CL = "Chile";
-  final static String RB = "Bolivia";
-  final static String PY = "Paraguay";
-  final static String BR = "Brazil";
-  final static String UY = "Uruguay";
-  
-  
-  
-  public Graph<String, DefaultEdge> returnMap() {
-    final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
-    this.myMap = myMap;
-    
+  static final String AR = "Argentina";
+  static final String CL = "Chile";
+  static final String RB = "Bolivia";
+  static final String PY = "Paraguay";
+  static final String BR = "Brazil";
+  static final String UY = "Uruguay";
+
+  final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
+
+  public Graph<String, DefaultEdge> createMap() {
+
     myMap.addVertex(AR);
     myMap.addVertex(CL);
     myMap.addVertex(RB);
     myMap.addVertex(PY);
     myMap.addVertex(BR);
     myMap.addVertex(UY);
-  
+
     myMap.addEdge(AR, CL);
     myMap.addEdge(AR, RB);
     myMap.addEdge(AR, PY);
     myMap.addEdge(AR, BR);
     myMap.addEdge(AR, UY);
-  
+
     return myMap;
   }
-  
 }
