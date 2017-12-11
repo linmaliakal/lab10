65bce8e2d397276e9192ff834114d5ed2aa101a9
linmaliakal <lmaliakal@luc.edu>
2017-12-11 00:54:10 +0000

    included country codes and added to map
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/edu/luc/cs271/countrygraph/CountryCodes.java
+++ src/main/java/edu/luc/cs271/countrygraph/CountryCodes.java
@@ -1,11 +1,28 @@
 package edu.luc.cs271.countrygraph;
 
+import java.util.Map;
+import java.util.*;
+
 public class CountryCodes {
 
-  static final String AR = "Argentina";
-  static final String CL = "Chile";
-  static final String RB = "Bolivia";
-  static final String PY = "Paraguay";
-  static final String BR = "Brazil";
-  static final String UY = "Uruguay";
+  final static String AR = "Argentina";
+  final static String CL = "Chile";
+  final static String RB = "Bolivia";
+  final static String PY = "Paraguay";
+  final static String BR = "Brazil";
+  final static String UY = "Uruguay";
+  
+  final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
+  myMap.addVertex(String AR);
+  myMap.addVertex(String CL);
+  myMap.addVertex(String RB);
+  myMap.addVertex(String PY);
+  myMap.addVertex(String BR);
+  myMap.addVertex(String UY);
+  
+  myMap.addEdge(AR, CL);
+  myMap.addEdge(AR, RB);
+  myMap.addEdge(AR, PY);
+  myMap.addEdge(AR, BR);
+  myMap.addEdge(AR, UY);
 }
