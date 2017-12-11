2df9165592392809e85c29cb216ee19d9c1123ae
linmaliakal <lmaliakal@luc.edu>
2017-12-11 21:37:00 +0000

    minor changes made to work it out
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/edu/luc/cs271/countrygraph/CountryCodes.java
+++ src/main/java/edu/luc/cs271/countrygraph/CountryCodes.java
@@ -1,7 +1,11 @@
 package edu.luc.cs271.countrygraph;
 
-import java.util.Map;
 import java.util.*;
+import java.awt.*;
+import java.net.*;
+import org.jgrapht.*;
+import org.jgrapht.alg.*;
+import org.jgrapht.traverse.*;
 
 public class CountryCodes {
 
@@ -12,17 +16,26 @@ public class CountryCodes {
   final static String BR = "Brazil";
   final static String UY = "Uruguay";
   
-  final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
-  myMap.addVertex(String AR);
-  myMap.addVertex(String CL);
-  myMap.addVertex(String RB);
-  myMap.addVertex(String PY);
-  myMap.addVertex(String BR);
-  myMap.addVertex(String UY);
   
-  myMap.addEdge(AR, CL);
-  myMap.addEdge(AR, RB);
-  myMap.addEdge(AR, PY);
-  myMap.addEdge(AR, BR);
-  myMap.addEdge(AR, UY);
+  
+  public Graph<String, DefaultEdge> returnMap() {
+    final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
+    this.myMap = myMap;
+    
+    myMap.addVertex(AR);
+    myMap.addVertex(CL);
+    myMap.addVertex(RB);
+    myMap.addVertex(PY);
+    myMap.addVertex(BR);
+    myMap.addVertex(UY);
+  
+    myMap.addEdge(AR, CL);
+    myMap.addEdge(AR, RB);
+    myMap.addEdge(AR, PY);
+    myMap.addEdge(AR, BR);
+    myMap.addEdge(AR, UY);
+  
+    return myMap;
+  }
+  
 }
