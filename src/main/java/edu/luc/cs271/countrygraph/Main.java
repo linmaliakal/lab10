d016453f2979f74d0f593d39be04b277faad11c2
linmaliakal <lmaliakal@luc.edu>
2017-12-12 00:38:11 +0000

    updated to print out map, add in color codes
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/edu/luc/cs271/countrygraph/Main.java
+++ src/main/java/edu/luc/cs271/countrygraph/Main.java
@@ -1,10 +1,24 @@
 package edu.luc.cs271.countrygraph;
 
 import java.util.Scanner;
+import java.awt.*;
+import java.net.*;
+import java.util.*;
+import org.jgrapht.*;
+import org.jgrapht.alg.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.traverse.*;
 
 public class Main {
 
-  public static void main(final String[] args) {
+  public static void main(final String[] args) throws InterruptedException{
     final Scanner input = new Scanner(System.in);
+    
+    Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
+    CountryCodes newMap = new CountryCodes(myMap);
+    myMap = newMap.createMap();
+    System.out.println(myMap);
+    
+    newMap.Breadth();
   }
 }
